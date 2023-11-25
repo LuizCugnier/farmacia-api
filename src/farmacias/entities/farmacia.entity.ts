036c23f903e028feeec67016766ae1efc002af9e
GutoCugnier <luizcugnier.neto@gmail.com>
2023-11-25 10:47:45 -0300

    entidade farmacia

---
+++ src/farmacias/entities/farmacia.entity.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Farmacia {
+    @PrimaryColumn()
+    id: number;
+
+    @Column()
+    logradouro: string;
+
+    @Column()
+    numero: number;
+
+    @Column()
+    bairro: string;
+
+    @Column()
+    cidade: string;
+
+    @Column()
+    estado: string;
+
+    constructor(props: {
+        id: number;
+        logradouro: string;
+        numero: number;
+        bairro: string;
+        cidade: string;
+        estado: string;
+    }) {
+        Object.assign(this, props);
+    }
+}
