67ff95745288d3316efb4c1639c7040248dc0a0c
GutoCugnier <luizcugnier.neto@gmail.com>
2023-11-26 16:06:49 -0300

    create module farmacia

---
+++ src/farmacias/farmacias.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { FarmaciasService } from './farmacias.service';
+import { FarmaciasController } from './farmacias.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Farmacia } from './entities/farmacia.entity';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Farmacia])],
+    controllers: [FarmaciasController],
+    providers: [FarmaciasService],
+})
+export class FarmaciasModule {}
