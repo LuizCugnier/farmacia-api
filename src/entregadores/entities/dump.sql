875160f3680eeb53b983d534880f488e06118a75
Lucas Borba <lucas.borba@hollox.com.br>
2023-12-04 19:53:25 -0300

    adicionados dumps para entregadores

---
+++ src/entregadores/entities/dump.sql
@@ -0,0 +1,7 @@
+--- CREATE TABLE ---
+CREATE TABLE entregador (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(255),
+  idade INTEGER,
+  cnh VARCHAR(255)
+);
---
+++ src/entregadores/use-case/dump.sql
@@ -0,0 +1,5 @@
+--- CREATE ---
+INSERT INTO entregador (nome, idade, cnh) VALUES ('valor_nome', valor_idade, 'valor_cnh');
+
+--- FIND ALL ---
+SELECT * FROM entregador;
