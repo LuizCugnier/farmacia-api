f9365cb0f0f6457d4ee65d5668a1251892b4459c
GutoCugnier <luizcugnier.neto@gmail.com>
2023-12-21 23:42:45 -0300

    Add validation to UpdateFarmaciaDto

--- src/farmacias/dto/update-farmacia.dto.ts
+++ src/farmacias/dto/update-farmacia.dto.ts
@@ -1,14 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { MaxLength } from 'class-validator';
 
 class _UpdateFarmaciaDto {
+  @MaxLength(150)
   logradouro: string;
 
+  @MaxLength(6)
   numero: number;
 
+  @MaxLength(100)
   bairro: string;
 
+  @MaxLength(100)
   cidade: string;
 
+  @MaxLength(100)
   estado: string;
 }
 
