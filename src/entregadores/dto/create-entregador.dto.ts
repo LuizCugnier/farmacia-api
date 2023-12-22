de2c5bd2b0d90d96ac4a9e5f44cc094014ed6265
GutoCugnier <luizcugnier.neto@gmail.com>
2023-12-21 23:42:55 -0300

    Update idade validation in DTOs

--- src/entregadores/dto/create-entregador.dto.ts
+++ src/entregadores/dto/create-entregador.dto.ts
@@ -2,10 +2,9 @@ import {
   IsNotEmpty,
   IsNumberString,
   MaxLength,
-  Min,
   Validate,
 } from 'class-validator';
-import { IsIdadeFormatConstraint } from 'src/validation/idade-format.validation';
+import { IsIdadeValidConstraint } from 'src/validation/idade-format.validation';
 
 export class CreateEntregadorDto {
   @IsNotEmpty({ message: 'O nome é obrigatório' })
@@ -13,9 +12,7 @@ export class CreateEntregadorDto {
   nome: string;
 
   @IsNotEmpty({ message: 'A idade é obrigatória' })
-  @IsNumberString({}, { message: 'A idade deve conter apenas números' })
-  @MaxLength(3, { message: 'A idade deve ter menos de 3 caracteres' })
-  @Validate(IsIdadeFormatConstraint)
+  @Validate(IsIdadeValidConstraint)
   idade: string;
 
   @IsNotEmpty({ message: 'CNH é obrigatória' })
--- src/entregadores/dto/update-entregador.dto.ts
+++ src/entregadores/dto/update-entregador.dto.ts
@@ -1,14 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { IsNumberString, MaxLength, Min, Validate } from 'class-validator';
-import { IsIdadeFormatConstraint } from 'src/validation/idade-format.validation';
+import { IsIdadeValidConstraint } from 'src/validation/idade-format.validation';
 
 class _UpdateEntregadorDto {
   @MaxLength(100, { message: 'O nome deve ter menos de 100 caracteres' })
   nome: string;
 
-  @IsNumberString({}, { message: 'A idade deve conter apenas números' })
-  @MaxLength(3, { message: 'A idade deve ter menos de 3 caracteres' })
-  @Validate(IsIdadeFormatConstraint)
+  @Validate(IsIdadeValidConstraint)
   idade: string;
 
   @MaxLength(9, { message: 'A CNH deve ter menos de 9 caracteres' })
